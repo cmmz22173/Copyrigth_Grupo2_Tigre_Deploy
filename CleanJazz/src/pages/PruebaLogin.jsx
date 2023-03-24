fcb12fe095c5bd2bf1816548c2c70e567665d061
M1S543L <dilan.almendarez@unah.hn>
2023-03-24 13:56:42 -0600

    Commit No tan importante, solo de prueba No descargar
    Para probar conexion con base de datos, sin emplementar Aun

---
+++ CleanJazz/src/pages/PruebaLogin.jsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import '../styles/Login.css'
+import logo from '../images/CleanJazz.png'
+import car from '../images/car1.jpg'
+import { Link } from "react-router-dom";
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      {new Date().getFullYear()}
+      {' Clean Jazz'}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function SignInSide() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    "Solo es Prueba"
+  );
+}
--- CleanJazz/src/router/Router.js
+++ CleanJazz/src/router/Router.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router,
  } from "react-router-dom";
  import Login from "../pages/Login"
  import Precios from '../pages/Precios'
+ import Prueba from "../pages/PruebaLogin"
 
  
 function Rutas() {
@@ -15,7 +16,7 @@ function Rutas() {
         <Route path="/login" element={ <Login/>}/>  
         <Route path="/" element={<Precios/>}/>
         <Route path="*" element={<h1>404 Not Found</h1>}/>
-        
+        <Route path="/prueba" element={<Prueba/>}/>
     </Routes>
 </Router>
   );
