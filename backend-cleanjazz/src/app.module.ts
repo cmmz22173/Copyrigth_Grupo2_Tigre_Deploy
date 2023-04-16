ac1c47706be8849db5ce9f2313dd83e40c27f501
M1S543L <dilan.almendarez@unah.hn>
2023-04-16 13:30:02 -0600

    Conexion a la base de datos
    Ya se conecta al aws el proyecto de nest

--- backend-cleanjazz/src/app.module.ts
+++ backend-cleanjazz/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'carwash-database-1.c4wwmskpgbep.us-east-1.rds.amazonaws.com',
+      username: 'tigre',
+      password: 'trestristestigrestragabantrigoen1trigal',
+      database: 'backoffice',
+      port: 3306,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      migrations: ['dist/migrations/*{.ts,.js}'],
+      migrationsRun: true,
+    }),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
